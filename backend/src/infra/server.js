df110bb67aae4dc36a5e75dd2700b5770e7459bc
victorhelopes <victorhelopes@gmail.com>
2024-08-07 21:52:21 -0300

    add: infra to database

--- backend/src/infra/server.js
+++ backend/src/infra/server.js
@@ -1,10 +1,15 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors")
 
+require('./database')
+
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 
 app.use(express.json());
 
-app.listen(3000);
+app.listen(port);
