63d547cc4ef76425f1bea7dc8b9624597214745e
victorhelopes <victorhelopes@gmail.com>
2024-08-08 09:00:41 -0300

    add: titleHeader component

---
+++ frontend/src/components/titleHeader/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1 class="mb-3">
+        {{ title }}
+    </h1>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TitleHeader',
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
+  
