95236b37efa8071b7b4bc2d2dd992af994a8d8a1
victorhelopes <victorhelopes@gmail.com>
2024-08-08 09:01:16 -0300

    add: section component

---
+++ frontend/src/components/section/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="elevation-10 pa-4">
+      <TitleHeader :title="title" />
+      <div class="content">
+        <slot></slot>
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  import TitleHeader from '../titleHeader';
+  
+  export default {
+    name: 'SectionComponent',
+    components: {
+      TitleHeader
+    },
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  }
+  </script>
